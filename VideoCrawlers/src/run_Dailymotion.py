7475a7be5edfe6a75197cec71de251393d27e1b4
tom3097 <tbochens@stud.elka.pw.edu.pl>
2016-03-09 09:52:27 +0100

    more efficient, +log, + one json, + user json

--- VideoCrawlers/src/run_Dailymotion.py
+++ VideoCrawlers/src/run_Dailymotion.py
@@ -3,64 +3,38 @@
 # Requirements:
 # Dailymotion sdk python:
 # https://github.com/dailymotion/dailymotion-sdk-python
-# youtube-dl:
-# https://github.com/rg3/youtube-dl
 
 from json import dumps
 from dailymotion import Dailymotion
-from youtube_dl import YoutubeDL
 from time import strftime
 from csv import reader
 from os import path
 from os import makedirs
-
-
-class DailymotionVideo:
-    def __init__(self, basic_metadata, stream_url):
-        self.info_dict = {
-            'id': basic_metadata['id'],
-            'allow_comments': basic_metadata['allow_comments'],
-            'available_formats': basic_metadata['available_formats'],
-            'aspect_ratio': basic_metadata['aspect_ratio'],
-            'bookmarks_total': basic_metadata['bookmarks_total'],
-            'comments_total': basic_metadata['comments_total'],
-            'country': basic_metadata['country'],
-            'created_time': basic_metadata['created_time'],
-            'duration': basic_metadata['duration'],
-            'description': basic_metadata['description'],
-            'sprite_320x_url': basic_metadata['sprite_320x_url'],
-            'embed_url': basic_metadata['embed_url'],
-            'filmstrip_60_url': basic_metadata['filmstrip_60_url'],
-            'language': basic_metadata['language'],
-            'owner_id': basic_metadata['owner.id'],
-            'tags': basic_metadata['tags'],
-            'thumbnail_url': basic_metadata['thumbnail_url'],
-            'title': basic_metadata['title'],
-            'url': basic_metadata['url'],
-            'views_last_day': basic_metadata['views_last_day'],
-            'views_last_hour': basic_metadata['views_last_hour'],
-            'views_last_month': basic_metadata['views_last_month'],
-            'views_last_week': basic_metadata['views_last_week'],
-            'views_total': basic_metadata['views_total'],
-            'current_date': strftime("%x"),
-            'current_time': strftime("%X"),
-            'source': stream_url,
-        }
 		
-    def __str__(self):
-        return dumps(self.info_dict, indent=4, sort_keys=True) + '\n'
 	
-    def save_to_file_oneline(self, file_desc):
-        jstr = dumps(self.info_dict, sort_keys=True) + '\n'
-        file_desc.write(jstr)
-		
-    def save_to_file_pretty(self, file_desc):
-        jstr = dumps(self.info_dict, indent=4, sort_keys=True) + '\n'
-        file_desc.write(jstr)
-		
-				
-def get_metadata(dailym, video_id):
-    fields = {
+def read_data_from_csv(id_file):
+    nameid_list = []
+    with open(id_file, 'r') as csvfile:
+        r = reader(csvfile)
+        for row in r:
+            nameid_list.append(row[1])
+    nameid_list.pop(0) 
+    return nameid_list
+
+    
+def get_user_data(dailymot, id):
+    user_parameters = {
+        'fields': 'followers_total,videos_total,videostar.bookmarks_total,views_total',
+    }  
+    try:
+        user_response = dailymot.get('/user/{}'.format(id), user_parameters)
+    except Exception:
+        return None
+    return user_response
+    
+
+def get_video_data(dailymot, id, current_page):
+    video_fields = {
         'id', 'title', 'thumbnail_url', 'tags', 'owner.id',
         'allow_comments', 'comments_total', 'bookmarks_total',
         'available_formats', 'views_last_day', 'views_last_hour',
@@ -68,72 +42,72 @@ def get_metadata(dailym, video_id):
         'country', 'created_time', 'duration', 'embed_url',
         'filmstrip_60_url','language', 'description',
         'sprite_320x_url', 'aspect_ratio', 'url',
-		
+    }
+    video_parameters = {
+        'fields': ','.join(video_fields),
+        'page': str(current_page),
+        'limit': '100',
     }
     try:
-        response = dailym.get('/video/{}'.format(video_id), {'fields': ','.join(fields)})
+        video_response = dailymot.get('/user/{}/videos'.format(id), video_parameters)
     except Exception:
         return None
-    return response
-	
-		
-def get_stream_url(embed_url, ydl_p):
-    try:
-        info = ydl_p.extract_info(embed_url, download=False)
-    except Exception:
-        return None
-    return info['url']
+    return video_response
 	
 
-def get_video_meta(dailymotion_id_file, dest_file):
+def start_crawling(id_file, log_file, video_file, user_file):
     dailymot = Dailymotion()
-    ydl = YoutubeDL({ 'quiet': 'True',})
-	
-    nameid_list = []
-    with open(dailymotion_id_file, "r") as csvfile:
-        r = reader(csvfile)
-        for row in r:
-            nameid_list.append(row[1])
-    nameid_list.pop(0)
-	
-    with open(dest_file, "w") as f:
-        for id in nameid_list:
-            print "Name: {}".format(id)
-            current_page = 1
-            for x in xrange(10):
-                parameters = {
-                    'fields': 'id',
-                    'page': str(current_page),
-                    'limit': '100',
-                }
-                try:
-                    response = dailymot.get("/user/{}/videos".format(id), parameters)
-                except Exception:
-                    continue
-
-                if "list" not in response:
-                    break
-	
-                for ele in response['list']:
-                    video_id = ele['id']
-                    print "Video id: {}".format(video_id)
-                    basic_metadata = get_metadata(dailymot, video_id)
-                    if basic_metadata is None:
-                        continue
-                    eurl = basic_metadata['embed_url']
-                    stream_url = get_stream_url(eurl, ydl)
-                    new_video = DailymotionVideo(basic_metadata, stream_url)
-                    new_video.save_to_file_pretty(f)
-				
-                current_page = current_page + 1
-                parameters['page'] = str(current_page)
-                has_more = response['has_more']
-                if has_more == False:
-                    break
-                
+    ydl = YoutubeDL({ 'quiet': 'True',})  
+    open(log_file, 'w').close()
+    open(video_file, 'w').close()
+    open(user_file, 'w').close() 
+    nameid_list = read_data_from_csv(id_file)  
+    dailymotion_videos = []
+    dailymotion_users = []    
+    for id in nameid_list:
+        user_response = get_user_data(dailymot, id)
+        if user_response is not None:
+            dailymotion_users.append(user_response)
+            total_user_videos = user_response['videos_total']
+            analyzed_videos = 0
+            with open(user_file, 'w') as f:
+                j_str = dumps(dailymotion_users, indent=4, sort_keys=True)
+                f.write(j_str)	        
+        current_page = 1
+        requests_counter = 0
+        while True:
+            video_response = get_video_data(dailymot, id, current_page)
+            if video_response is None:
+                break
+            if 'list' not in video_response:
+                break
+            analyzed_videos = analyzed_videos + len(video_response['list'])
+            prog = float(analyzed_videos) / float(total_user_videos)
+            log = '[{}] From: {} Responce size: {} Progress:{}\n'.format(strftime('%x %X'), id, len(video_response['list']), prog)
+            with open(log_file, 'a') as f:
+                f.write(log)
+            for ele in video_response['list']:
+                ele['date_time'] = strftime('%x %X')
+                dailymotion_videos.append(ele) 
+            requests_counter = requests_counter + 1
+            if requests_counter == 10:
+                requests_counter = 0
+                with open(video_file, "w") as f:
+                    j_str = dumps(dailymotion_videos, indent=4, sort_keys=True)
+                    f.write(j_str)          
+            current_page = current_page + 1
+            has_more = video_response['has_more']
+            if has_more == False:
+                break
+    with open(video_file, "w") as f:
+        j_str = dumps(dailymotion_videos, indent=4, sort_keys=True)
+        f.write(j_str)               
 		
+        
 if __name__ == "__main__":
     if not path.exists('../results'):
         makedirs('../results')
-    get_video_meta('../data/dailymotion.csv', '../results/dailymotion_res.json')
-
+    if not path.exists('../logs'):
+        makedirs('../logs')
+    start_crawling('../data/dailymotion.csv', '../logs/dailymotion_logs.log', '../results/dailymotion_video.json', '../results/dailymotion_users.json')
+    
